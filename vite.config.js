15808e473b4ce834e10ab28b220d22902c0db69f
dragneel <sayakascent@gmail.com>
2025-06-15 00:02:11 +0530

    webApp

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increases the warning threshold
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          firebase: ['firebase', '@supabase/supabase-js'],
+          mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
+          utils: ['clsx', 'sweetalert2', 'framer-motion', 'aos']
+        }
+      }
+    }
+  }
 })
